f8d3aa4af556e4bbf9a6972191d910823861ff40
MazdaAzda <114338933+MazdaAzda@users.noreply.github.com>
2022-10-05 01:19:18 +0700

    Add files via upload

---
+++ Lat4_2b.php
@@ -0,0 +1,4 @@
+<?php
+require_once("lat4_2a.php");
+$mhs2 = new mahasiswa("Mazda Nawallsyah", "G.211.21.0082", "F T I K", "Semester 3");
+$mhs2->cetak();
---
+++ Lat4_3b.php
@@ -0,0 +1,7 @@
+<?php
+require_once("lat4_3a.php");
+$mhs1 = new mahasiswa();
+$mhs1->nama = "<b>Mazda Nawallsyah</b></br></br>";
+$mhs1->nim = "G.211.21.0082";
+echo $mhs1->nama;
+echo $mhs1->nim;
